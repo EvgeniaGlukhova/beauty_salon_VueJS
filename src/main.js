ca772613f262ddf6aaa31e7abe7df1bfbe70b75a
Evgenia Glukhova <glukhova_ea@edu.surgu.ru>
2025-10-05 17:06:21 +0500

    PrimeVue

--- src/main.js
+++ src/main.js
@@ -4,12 +4,31 @@ import App from './App.vue'
 
 import { createPinia } from 'pinia'
 
+import router from './router.js'
 
-import router from '@/router.js'
+import PrimeVue from 'primevue/config';
+
+import Aura from '@primevue/themes/aura';
+
+import 'primeicons/primeicons.css';
+
+import './style.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+});
 
 app.mount('#app')
